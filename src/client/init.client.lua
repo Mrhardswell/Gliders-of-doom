d92a40166a6ee2bbcad60bfcb34d80f0f49d83c8
MrHardswell <50495373+Mrhardswell@users.noreply.github.com>
2023-11-16 00:52:53 -0400

    Nightly Basic Setup

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,17 @@
-print("Hello world, from client!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+Knit.Components = {}
+Knit.Component = require(ReplicatedStorage.Packages.Component)
+
+Knit.Player = game.Players.LocalPlayer
+
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+
+for _, component in script.Components:GetChildren() do
+    Knit.Components[component.Name] = require(component)
+end
+
+Knit.AddControllers(script.Controllers)
+Knit.Start():catch(warn)
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,22 @@
-print("Hello world, from server!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+local Net = require(ReplicatedStorage.Packages.Net)
+
+Knit.Component = require(ReplicatedStorage.Packages.Component)
+Knit.Private = require(ServerStorage.Private)
+
+local Message = Net:RemoteEvent("ServerMessage")
+
+Knit.Components = {}
+
+Knit.ServerMessage = Message
+
+for _, Component in script.Components:GetChildren() do
+    if not Component:IsA("ModuleScript") then continue end
+    Knit.Components[Component.Name] = require(Component)
+end
+
+Knit.AddServices(script.Services)
+Knit.Start():catch(warn)
